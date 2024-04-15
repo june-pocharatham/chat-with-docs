2233266b4426d377d8f726730144d05c593f3fa2
Pocharatham (June) Sinlapanuntakul <psinlapanuntakul@harmonia.com>
2024-04-15 00:42:31 -0400

    Added Q&A feature

--- app.py
+++ app.py
@@ -16,22 +16,39 @@ def main():
     
     # Page configurations
     st.set_page_config(page_title="Chat App", page_icon="📖", layout="centered", initial_sidebar_state="expanded")
+
     
+    # Session state initializations
+    if "collection_id" not in st.session_state:
+        st.session_state.collection_id = client.create_collection(name="Collection_" + str(uuid.uuid4()), description="")
+    
+    if "chat_session_id" not in st.session_state:
+        st.session_state.chat_session_id = client.create_chat_session(st.session_state.collection_id)
+    
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = []  
+        
+    st.write(st.session_state)    # degugging
+    
+    
+    
+        
+        
     st.title(body="Chat with your documents :book:", anchor=False)
+    
+
+        
     
     # Sidebar configurations
     with st.sidebar:
         st.subheader("Choose file(s) to upload")
-        uploaded_files_st = st.file_uploader("Upload your documents here", accept_multiple_files=True, key="sidebar_page", label_visibility="collapsed")
-        upload_files_h2o_button = st.button("Upload :arrow_up:", key="button_upload_docs")            
-    
+        uploaded_files_st = st.file_uploader("Upload your documents here", accept_multiple_files=True, label_visibility="collapsed")
+        upload_files_h2o_button = st.button("Upload :arrow_up:")            
+        
         if uploaded_files_st is not None and upload_files_h2o_button:
             st.divider()
-            st.subheader("Upload Status", anchor=False)
-            with st.spinner("Starting processing documents..."):
-                # Create a collection in h2ogpte
-                collection_id = client.create_collection(name="Collection_" + str(uuid.uuid4()), description="")
-                
+            st.subheader("Upload status", anchor=False)
+                                
             with st.spinner("Uploading documents..."):
                 # Upload files to h2ogpte backend
                 upload_h2o_ids = []
@@ -40,15 +57,40 @@ def main():
 
             with st.spinner("Ingesting documents..."):
                 # Upload backend files to collection
-                client.ingest_uploads(collection_id=collection_id,upload_ids=upload_h2o_ids)
+                client.ingest_uploads(collection_id=st.session_state.collection_id, upload_ids=upload_h2o_ids)
                 
             st.success("Successfully processed your documents", icon="✅")
-        
-        
-        
+            
+    st.markdown(st.session_state.collection_id)
     
+    # Chat feature
+    user_query = st.chat_input("Ask a question...")
+    if user_query:
+        
+        # Display user query
+        with st.chat_message("user"):
+            st.markdown(user_query)
+        
+        with client.connect(st.session_state.chat_session_id) as chat_session:
+            response = chat_session.query(
+                message=user_query,
+                llm="gpt-4-1106-preview",
+                rag_config={"rag_type": "rag"},
+                timeout=60
+            )
+            # Display LLM response
+            with st.chat_message("ai"):
+                st.markdown(response.content)
+            
+            
+            
+    
+   
     # Delete the collection in h2ogpte
     # client.delete_collections(collection_ids=[collection_id], timeout=30)
+
+
+
 
 if __name__ == "__main__":
     main()
