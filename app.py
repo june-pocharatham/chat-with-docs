72573661060d1d619d483484b97140a70477df9a
junepochara <psinlapanuntakul@harmoni.com>
2024-04-24 22:27:01 -0400

    Added a sample doc

--- app.py
+++ app.py
@@ -5,17 +5,10 @@ from dotenv import load_dotenv
 import uuid
 
 
-def main():
-    load_dotenv()
-    try:
-        client = H2OGPTE(
-            address=os.getenv("H2OGPTE_ADDRESS"),
-            api_key=os.getenv("H2OGPTE_GLOBAL_API_KEY"),
-        )
-    except Exception as e:
-        print(e)
-
-    # Page configurations
+def set_page_config():
+    """
+    Page configurations
+    """
     st.set_page_config(
         page_title="Chat App",
         page_icon="📖",
@@ -23,7 +16,11 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    # Session state initializations
+
+def initialize_session_state(client: H2OGPTE):
+    """
+    Session state initializations
+    """
     if "collection_id" not in st.session_state:
         st.session_state.collection_id = client.create_collection(
             name="Collection_" + str(uuid.uuid4()), description=""
@@ -40,10 +37,18 @@ def main():
     if "uploaded_docs" not in st.session_state:
         st.session_state.uploaded_docs = []
 
-    # Page details
+
+def display_page_details():
+    """
+    Display page details: title
+    """
     st.title(body="Chat with your documents :book:", anchor=False)
 
-    # Sidebar configurations
+
+def display_sidebar(client: H2OGPTE):
+    """
+    Sidebar configurations
+    """
     with st.sidebar:
         st.subheader("Choose file(s) to upload")
         uploaded_files_st = st.file_uploader(
@@ -94,7 +99,11 @@ def main():
             if clear_chat_button:
                 st.session_state.conversation.clear()
 
-    # Display conversation history in session state
+
+def display_convo_history():
+    """
+    Display conversation history in session state
+    """
     for conversation in st.session_state.conversation:
         if conversation["role"] == "user":
             with st.chat_message("user"):
@@ -103,33 +112,81 @@ def main():
             with st.chat_message("ai"):
                 st.markdown(conversation["message"])
 
-    # Chat feature
+
+def display_chat(client: H2OGPTE):
     user_query = st.chat_input("Ask a question...")
     if user_query:
-
         # Display user query
         with st.chat_message("user"):
             st.markdown(user_query)
         st.session_state.conversation.append({"role": "user", "message": user_query})
 
+        # Check number of documents in the collection
+        num_docs_in_collection = client.count_documents_in_collection(
+            st.session_state.collection_id
+        )
+
+        # Assign rag approach
+        if num_docs_in_collection > 0:
+            rag_approach = "rag"
+        else:
+            rag_approach = "llm_only"
+
         with client.connect(st.session_state.chat_session_id) as chat_session:
             response = chat_session.query(
                 message=user_query,
                 llm="gpt-4-1106-preview",
-                rag_config={"rag_type": "rag"},
-                timeout=60,
+                llm_args={
+                    "temperature": 0,
+                },
+                rag_config={
+                    "rag_type": rag_approach,
+                },
+                timeout=None,
             )
             # Display LLM response
             with st.chat_message("ai"):
                 st.markdown(response.content)
             st.session_state.conversation.append(
-                {"role": "ai", "message": response.content}
+                {
+                    "role": "ai",
+                    "message": response.content,
+                }
             )
 
-    st.session_state  # display session state for degugging
+
+def main():
+    load_dotenv()
+    try:
+        client = H2OGPTE(
+            address=os.getenv("H2OGPTE_ADDRESS"),
+            api_key=os.getenv("H2OGPTE_GLOBAL_API_KEY"),
+        )
+    except Exception as e:
+        print(f"ERROR DETAILS ({e})")
+
+    # Page configurations
+    set_page_config()
+
+    # Session state initializations
+    initialize_session_state(client)
+
+    # Page details
+    display_page_details()
+
+    # Sidebar configurations
+    display_sidebar(client)
+
+    # Display conversation history in session state
+    display_convo_history()
+
+    # Chat feature
+    display_chat(client)
+
+    # st.session_state  # display session state for degugging
 
     # Delete the collection in h2ogpte
-    # client.delete_collections(collection_ids=[collection_id], timeout=30)
+    # client.delete_collections([st.session_state.collection_id])
 
 
 if __name__ == "__main__":
